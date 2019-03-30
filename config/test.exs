bb006c230b7eeaa3d7ce9392a3add13bb61d9a82
Maneesh Babu M <maneesh@amagi.com>
2019-03-30 14:11:48 +0530

    update layout (#1)
    * install and configure materializecss
    
    * update layout
    
    * update test configs

--- config/test.exs
+++ config/test.exs
@@ -12,7 +12,7 @@ config :logger, level: :warn
 # Configure your database
 config :chirper, Chirper.Repo,
   username: "postgres",
-  password: "postgres",
+  password: "welcome",
   database: "chirper_test",
   hostname: "localhost",
   pool: Ecto.Adapters.SQL.Sandbox
