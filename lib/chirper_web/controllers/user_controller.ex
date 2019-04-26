05c35d518efc3a56bd6951b8b5a95d0216432969
Maneesh Babu M <maneeshbabum@gmail.com>
2019-04-26 16:59:43 +0530

    add session

--- lib/chirper_web/controllers/user_controller.ex
+++ lib/chirper_web/controllers/user_controller.ex
@@ -11,10 +11,11 @@ defmodule ChirperWeb.UserController do
 
   def create(conn, %{"user" => user_params}) do
     case Accounts.create_user(user_params) do
-    {:ok, user} ->
-      conn
-      |> put_flash(:info, "Signed up successfully.")
-      |> redirect(to: "/")
+      {:ok, user} ->
+        conn
+        |> put_session(:current_user_id, user.id)
+        |> put_flash(:info, "Signed up successfully.")
+        |> redirect(to: Routes.page_path(conn, :index))
     {:error, %Ecto.Changeset{} = changeset} ->
       render(conn, "new.html", changeset: changeset)
     end
