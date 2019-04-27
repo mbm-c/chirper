2515948ab96c425c45287c56cc4051503670fca5
Maneesh Babu M <maneeshbabum@gmail.com>
2019-04-27 21:31:34 +0530

    fix tests

--- test/chirper_web/controllers/user_controller_test.exs
+++ test/chirper_web/controllers/user_controller_test.exs
@@ -15,7 +15,7 @@ defmodule ChirperWeb.UserControllerTest do
   describe "new user" do
     test "renders form", %{conn: conn} do
       conn = get(conn, Routes.user_path(conn, :new))
-      assert html_response(conn, 200) =~ "New User"
+      assert html_response(conn, 200) =~ "REGISTER"
     end
   end
 
@@ -27,7 +27,7 @@ defmodule ChirperWeb.UserControllerTest do
 
     test "renders errors when data is invalid", %{conn: conn} do
       conn = post(conn, Routes.user_path(conn, :create), user: @invalid_attrs)
-      assert html_response(conn, 200) =~ "New User"
+      assert html_response(conn, 200) =~ "REGISTER"
     end
   end
 
