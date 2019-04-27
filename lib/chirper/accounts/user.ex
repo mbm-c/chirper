ae909642aafa8a191b7c871c9519b73eaecd0d27
Maneesh Babu M <maneeshbabum@gmail.com>
2019-04-27 22:55:13 +0530

    add user password confirmation

--- lib/chirper/accounts/user.ex
+++ lib/chirper/accounts/user.ex
@@ -1,22 +1,45 @@
 defmodule Chirper.Accounts.User do
   use Ecto.Schema
   import Ecto.Changeset
-  alias Chirper.Accounts.User
-  alias Comeonin.Bcrypt
+  alias Chirper.Accounts.{User, Encryption}
 
   schema "users" do
     field :encrypted_password, :string
     field :username, :string
 
+    # VIRTUAL FIELDS
+    field :password, :string, virtual: true
+    field :password_confirmation, :string, virtual: true
+
     timestamps()
   end
+
+  @required_fields ~w(username password)
+  @optional_fields ~w()
 
   @doc false
   def changeset(%User{} = user, attrs) do
     user
-    |> cast(attrs, [:username, :encrypted_password])
+    |> cast(attrs, @required_fields, @optional_fields)
+    |> validate_required([:username])
+    |> validate_length(:password, min: 6)
+    |> validate_confirmation(:password)
     |> unique_constraint(:username)
-    |> validate_required([:username, :encrypted_password])
-    |> update_change(:encrypted_password, &Bcrypt.hashpwsalt/1)
+    |> downcase_username
+    |> encrypt_password
+  end
+
+  defp encrypt_password(changeset) do
+    password = get_change(changeset, :password)
+    if password do
+      encrypted_password = Encryption.hash_password(password)
+      put_change(changeset, :encrypted_password, encrypted_password)
+    else
+      changeset
+    end
+  end
+
+  defp downcase_username(changeset) do
+    update_change(changeset, :username, &String.downcase/1)
   end
 end
